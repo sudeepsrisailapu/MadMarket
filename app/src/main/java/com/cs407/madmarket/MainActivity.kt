8805766c15ecaf9024083290a0d920a9e83419df
Debs <nanakwamedebrah1@gmail.com>
2024-11-09 03:28:54 -0600

    Added navigation

--- app/src/main/java/com/cs407/madmarket/MainActivity.kt
+++ app/src/main/java/com/cs407/madmarket/MainActivity.kt
@@ -5,16 +5,53 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.cs407.madmarket.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        replaceFragment(HomeFragment())
+
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment())
+                }
+
+                R.id.search -> {
+                    replaceFragment(SearchFragment())
+                }
+
+                R.id.cart -> {
+                    replaceFragment(CartFragment())
+                }
+
+                R.id.profile -> {
+                    replaceFragment(ProfileFragment())
+                }
+
+                R.id.messages -> {
+                    replaceFragment(MessagesFragment())
+                }
+            }
+
+
+            return@setOnItemSelectedListener true
         }
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment::class.java, null)
+            .setReorderingAllowed(true).addToBackStack("Added " + fragment.toString()).commit()
+    }
+
 }
