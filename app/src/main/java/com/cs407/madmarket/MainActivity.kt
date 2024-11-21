c6e36c2a4b7e2904ed038aac75776e2b2bb05719
vinhdang2 <vjdang@wisc.edu>
2024-11-21 17:09:20 -0600

    notifications permissions

--- app/src/main/java/com/cs407/madmarket/MainActivity.kt
+++ app/src/main/java/com/cs407/madmarket/MainActivity.kt
@@ -1,13 +1,43 @@
 package com.cs407.madmarket
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.cs407.madmarket.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    //get permissions for notifications
     private lateinit var binding: ActivityMainBinding
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {isGranted: Boolean ->
+        if (!isGranted){
+            Toast.makeText(this, "Please allow all notifications to continue.",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+
+    @VisibleForTesting
+    public fun requestPermission(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+
+
+            if (ContextCompat.checkSelfPermission(
+                    applicationContext, android.Manifest.permission.POST_NOTIFICATIONS
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
